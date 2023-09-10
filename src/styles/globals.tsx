3b4152f19081e918085844009186626cd3cbf5b8
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-09-10 20:05:53 +0900

    💄: Update Global style

--- src/styles/globals.tsx
+++ src/styles/globals.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { Global, css } from "@emotion/react";
+import React from 'react'
+import { Global, css } from '@emotion/react'
 
 const Theme = `
 
-`;
-
+`
 
 const style = css`
-    
-`;
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: Pretendard;
+  }
+`
 
 const GlobalStyle = () => {
-    return <Global styles={style} />;
-  };
-  
-  export default GlobalStyle;
+  return <Global styles={style} />
+}
+
+export default GlobalStyle
