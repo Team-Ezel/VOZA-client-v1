7c31d7a91d6855fc82f0753ce6e887bb179f30d0
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-09-11 23:12:50 +0900

    🍱 : Add OtherClass FIlter Data

---
+++ src/assets/data/OtherClassFilterListData.ts
@@ -0,0 +1,40 @@
+
+
+export const OtherClassFilterListData = [
+  {
+    name: '전체',
+    value: 'all',
+  },
+  {
+    name: '취미',
+    value: 'hobby'
+  },
+  {
+    name: '스포츠',
+    value: 'sports',
+  },
+  {
+    name: '문화/예술',
+    value: 'art',
+  },
+  {
+    name: '친목/모임',
+    value: 'amity',
+  },
+  {
+    name: '여행',
+    value: 'travel',
+  },
+  {
+    name: '공부',
+    value: 'study',
+  },
+  {
+    name: '반려동물',
+    value: 'pet',
+  },
+  {
+    name: '건강',
+    value: 'health',
+  },
+]
--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,2 +1,3 @@
 export { default as Default_image } from './Default_image';
 export { default as Add_circle } from './Add_ circle';
+export { default as Arrow_down_dill } from './Arrow_down_fill';
