535bbe25a8b9e95fda4561b3bb9a62286e35da39
윤수용 <yunsuyong739@naver.com>
2023-09-27 12:19:49 +0900

    :bento: Add assets

---
+++ src/assets/svgs/Out.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Out = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='16'
+      height='16'
+      viewBox='0 0 16 16'
+      fill='none'
+    >
+      <path
+        d='M8.77778 8V8.00778M1 15H15M2.55556 15V2.55556C2.55556 2.143 2.71944 1.74733 3.01117 1.45561C3.30289 1.16389 3.69855 1 4.11111 1H9.94444M11.8889 9.16667V15M9.55556 4.11111H15M15 4.11111L12.6667 1.77778M15 4.11111L12.6667 6.44444'
+        stroke='#FF3120'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  )
+}
+
+export default Out
