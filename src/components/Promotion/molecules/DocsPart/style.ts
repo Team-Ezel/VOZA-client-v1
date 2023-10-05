27639b2ffea9adb190a88457ba64fd3e4fe7dfee
sanadevel <a36133353@gmail.com>
2023-10-05 11:24:44 +0900

    :bug: Fix Responsive Style

--- src/components/Promotion/molecules/DocsPart/style.ts
+++ src/components/Promotion/molecules/DocsPart/style.ts
@@ -3,8 +3,45 @@ import styled from '@emotion/styled'
 export const TitleText = styled.h1`
   font-size: 50px;
   padding-bottom: 1rem;
+  @media (max-width: 1634px) {
+    font-size: 40px;
+    // 노트북
+  }
+
+  @media (max-width: 950px) {
+    font-size: 30px;
+    // 테블릿
+  }
+
+  @media (max-width: 420px) {
+    font-size: 20px;
+    // 폰
+  }
+
+  @media (min-width: 1920px) {
+    font-size: 50px;
+    // 모니터
+  }
 `
 
 export const DetailText = styled.p`
   font-size: 20px;
+  @media (max-width: 1634px) {
+    font-size: 16px;
+    // 노트북
+  }
+
+  @media (max-width: 950px) {
+    font-size: 12px;
+    // 테블릿
+  }
+
+  @media (max-width: 420px) {
+    font-size: 8px;
+    // 폰
+  }
+
+  @media (min-width: 1950px) {
+    // 모니터
+  }
 `
