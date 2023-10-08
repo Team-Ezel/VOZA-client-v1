e1158560f5b39b644d322a31cab7869ebcecf03f
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-10-08 20:06:37 +0900

    :lipstick: : Add hook useModal

---
+++ src/hooks/useModal.tsx
@@ -0,0 +1,26 @@
+import { useCallback } from 'react'
+import { modalState } from '@/atoms/atoms'
+import { useRecoilState } from 'recoil'
+
+export default function useModal() {
+  const [modal, setModal] = useRecoilState(modalState)
+
+  const openModal = useCallback(
+    (modalData: React.ReactNode) => {
+      setModal({
+        component: modalData,
+        visible: true,
+      })
+    },
+    [setModal],
+  )
+
+  const closeModal = useCallback(() => {
+    setModal({
+      component: null,
+      visible: false,
+    })
+  }, [setModal])
+
+  return { openModal, closeModal, visible: !modal.visible }
+}
