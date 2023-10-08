6fdf2c60195566d25ea9905fd3f287cecbfa3c19
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-10-08 19:54:55 +0900

    :lipstick: : Add modalState atom

--- src/atoms/atoms.ts
+++ src/atoms/atoms.ts
@@ -1,5 +1,6 @@
 import { atom } from "recoil";
 import { NewClassModalStateType } from "@/types/components/common/NewClassModal"
+import { ModalStateType } from "@/types/hooks/modalStateType";
 
 export const newClassModal = atom<NewClassModalStateType>({
     key:"NewClassModal",
@@ -15,4 +16,12 @@ export const newClassModal = atom<NewClassModalStateType>({
 export const otherClassFilter = atom({
    key:"otherClassFilter",
    default:"all"
+})
+
+export const modalState = atom<ModalStateType>({
+    key:'modalState',
+    default: {
+        component:null,
+        visible: false,
+    }
 })
---
+++ src/types/hooks/modalStateType.ts
@@ -0,0 +1,4 @@
+export type ModalStateType = {
+    component:React.ReactNode
+    visible: boolean
+}
