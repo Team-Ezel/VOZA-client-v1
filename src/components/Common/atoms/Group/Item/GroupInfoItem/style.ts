ed612ef01baef703fc93a7e1886368ec558301b4
윤수용 <yunsuyong739@naver.com>
2023-10-09 16:25:34 +0900

    :lipstick: Update GroupInfo Components

---
+++ src/components/Common/atoms/Group/Item/GroupInfoItem/style.ts
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled'
+
+export const InfoContainer = styled.div`
+  width: 18.5625rem;
+  height: 16rem;
+  flex-shrink: 0;
+
+  margin-top: 100px;
+  padding-top: 55px;
+  padding-left: 100px;
+`
+
+export const ImgContainer = styled.div`
+  display: flex;
+  width: 16rem;
+  height: 12rem;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 0.9375rem;
+`
+
+export const GroupName = styled.div`
+  margin-top: 10px;
+  color: #000;
+  font-family: Pretendard;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`
