2af66b52e0790311fa7416c3fbd785d7952ea4ab
sanadevel <a36133353@gmail.com>
2023-10-17 15:48:33 +0900

    :bento: BoardPage add Assets

---
+++ src/assets/svgs/NoVoteSvg.tsx
@@ -0,0 +1,32 @@
+const NoVoteSvg = () => {
+  return (
+    <svg
+      width='87'
+      height='87'
+      viewBox='0 0 87 87'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M18.125 25.375C18.125 21.3875 21.3875 18.125 25.375 18.125H61.625C63.5478 18.125 65.3919 18.8888 66.7515 20.2485C68.1112 21.6081 68.875 23.4522 68.875 25.375V68.875H18.125V25.375ZM79.75 68.875H7.25H79.75Z'
+        fill='#ADB1BD'
+      />
+      <path
+        d='M79.75 68.875H7.25M18.125 25.375C18.125 21.3875 21.3875 18.125 25.375 18.125H61.625C63.5478 18.125 65.3919 18.8888 66.7515 20.2485C68.1112 21.6081 68.875 23.4522 68.875 25.375V68.875H18.125V25.375Z'
+        stroke='#656B80'
+        stroke-width='5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+      <path
+        d='M32.625 43.5L39.875 50.75L54.375 36.25'
+        stroke='#656B80'
+        stroke-width='5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  )
+}
+
+export default NoVoteSvg
