a2e26c969612290e4ecb513694f66cb8971e6be9
이아론 <aron0613@naver.com>
2023-10-17 16:16:52 +0900

    :bento: Update asset index

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -11,3 +11,8 @@ export { default as Add_circle } from './Add_ circle'
 export { default as Arrow_down_dill } from './Arrow_down_fill'
 export { default as Camera } from './Camera'
 export { default as Arrow_down_2 } from "./Arrow_down_2"
+
+export { default as Arrow_left } from "./Arrow_left"
+export { default as Arrow_right } from "./Arrow_right"
+
+export { default as Add } from "./Add"
