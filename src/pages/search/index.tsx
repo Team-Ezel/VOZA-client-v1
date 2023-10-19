3306a0cf0d1dcc830a24bf56c2870494e54666cb
sanadevel <a36133353@gmail.com>
2023-10-19 17:41:24 +0900

    :sparkles: Feat(LoginCheck) : 로그인 체크 훅 생성

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,11 @@
+import SearchTemplates from '@/components/Search/templates'
+
+const Search = () => {
+  return (
+    <>
+      <SearchTemplates></SearchTemplates>
+    </>
+  )
+}
+
+export default Search
