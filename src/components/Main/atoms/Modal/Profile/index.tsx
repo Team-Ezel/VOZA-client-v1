1659e4fcff27d2f25adbb77341d31c6bd48db2c4
이아론 <aron0613@naver.com>
2023-10-24 23:47:41 +0900

    :lipstick: Update Image preview

--- src/components/Main/atoms/Modal/Profile/index.tsx
+++ src/components/Main/atoms/Modal/Profile/index.tsx
@@ -1,20 +1,64 @@
-import Button from '@/components/Common/atoms/Button/Button'
-import { useRecoilState } from 'recoil'
-import { newClassModal } from '@/atoms/atoms'
-import { NewClassModalStateType } from '@/types/components/common/NewClassModal'
-import * as S from './style'
-import * as I from '@/assets/svgs'
-import { useRef } from 'react'
+import Button from '@/components/Common/atoms/Button/Button';
+import { useRecoilState } from 'recoil';
+import { newClassModal, newClassState } from '@/atoms/atoms';
+import { NewClassModalStateType } from '@/types/components/common/NewClassModal';
+import * as S from './style';
+import * as I from '@/assets/svgs';
+import { useRef, useState } from 'react';
+import useFetchFormdata from '@/hooks/useFetchFormdata';
+
 function Profile() {
   const [NewClassModalState, setNewClassModalState] =
-    useRecoilState<NewClassModalStateType>(newClassModal)
-  const fileRef = useRef(null)
-  const handleUpload = () => { }
+    useRecoilState<NewClassModalStateType>(newClassModal);
+  const [newclassState, setNewclassState] = useRecoilState(newClassState);
+  const [imgFile, setImgFile] = useState<string | ArrayBuffer | null>(""); // 초기 상태는 빈 문자열 또는 ArrayBuffer로 설정
+  const imgRef = useRef<HTMLInputElement | null>(null); // useRef에 타입 추가
+
+  const { isLoading, fetch } = useFetchFormdata({
+    url: "/group",
+    method: "post"
+  })
+
+  const handleUpload = () => {
+    if (imgRef.current && imgRef.current.files && imgRef.current.files[0]) {
+      const file = imgRef.current.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setImgFile(reader.result);
+      };
+    }
+  }
+
+  const handleClick = () => {
+    let formData = new FormData();
+    formData.append('data', JSON.stringify({ ...newclassState }));
+    formData.append('file', { imgFile })
+    for (let key of formData.keys()) {
+      console.log(key);
+    }
+
+    /* value 확인하기 */
+    for (let value of formData.values()) {
+      console.log(value);
+    }
+    fetch(formData)
+
+    setNewClassModalState({
+      ...NewClassModalState,
+      page: 'category',
+      modal: false,
+    });
+
+
+  }
 
   return (
     <S.ClassProfileWrapper>
       <S.ProfileWrapper>
-        <S.Profile />
+        <S.Profile>
+          <img src={imgFile || ''} alt='' />
+        </S.Profile>
         <S.ProfileButtonWrapper>
           <S.AddProfileButton htmlFor='file'>
             <I.Camera />
@@ -25,7 +69,8 @@ function Profile() {
             multiple
             type='file'
             id='file'
-            ref={fileRef}
+            ref={imgRef}
+            name='profileImg'
             onChange={handleUpload}
           />
         </S.ProfileButtonWrapper>
@@ -38,16 +83,12 @@ function Profile() {
         background='#3355CD'
         color='#fff'
         fontSize='18px'
-        onClick={() => {
-          setNewClassModalState({
-            ...NewClassModalState,
-            profile: '',
-            page: 'category',
-            modal: false,
-          })
-        }}
-      >다음</Button>
+        onClick={handleClick} // handleUpload 대신 handleClick를 호출
+      >
+        다음
+      </Button>
     </S.ClassProfileWrapper>
-  )
+  );
 }
-export default Profile
+
+export default Profile;
