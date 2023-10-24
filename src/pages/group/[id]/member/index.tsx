d7c78980ef33fee64d14391dbda6a03ff41e1c80
윤수용 <yunsuyong739@naver.com>
2023-10-24 20:43:01 +0900

    :art: Move MemberPage file

--- src/pages/group/[id]/member/index.tsx
+++ src/pages/group/[id]/member/index.tsx
@@ -1,8 +1,8 @@
 import Header from '@/components/Common/organisms/Header'
 import React from 'react'
-import * as S from '../../../components/Member/templates/GroupMemberTemplates/style'
 import GroupCategories from '@/components/Common/organisms/GroupCategories'
 import MemberOrganisms from '@/components/Member/organisms/Memberorganisms'
+import * as S from '@/components/Member/templates/GroupMemberTemplates/style'
 
 
 function Member() {
