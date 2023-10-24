2e6d58f3ff995d53128a2d37a1fcc56362c1af9d
윤수용 <yunsuyong739@naver.com>
2023-10-24 20:43:57 +0900

    :necktie: Add : 그룹썸네일 및 그룹이름 불러오는 로직 구현

--- src/components/Member/organisms/Memberorganisms/index.tsx
+++ src/components/Member/organisms/Memberorganisms/index.tsx
@@ -1,14 +1,49 @@
 import GroupInfoItem from '@/components/Common/atoms/Group/Item/GroupInfoItem'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MemberList from '../../molecules/MemberList'
+import { useRouter } from 'next/router'
+import axios from 'axios'
 
-const MemberOrganisms = () => {
+interface Group {
+  url: string
+  groupName: string
+}
+
+const MemberOrganisms: React.FC = () => {
+  const baseurl: string | undefined = process.env.NEXT_PUBLIC_BASEURL
+  const router = useRouter()
+  const { id } = router.query
+  const [group, setGroup] = useState<Group | null>(null)
+
+  const fetchData = async () => {
+    try {
+      const accessToken = localStorage.getItem('kakao-accessToken')
+
+      if (!accessToken) {
+        console.error('Access Token이 없습니다.')
+        return
+      }
+
+      const response = await axios.get<Group>(`${baseurl}/group/detail/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      setGroup(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    if (id) {
+      fetchData()
+    }
+  }, [id])
+
   return (
     <>
-      <GroupInfoItem
-        url='https://s3.ap-northeast-2.amazonaws.com/gimuwiki/file/c5c1e8ce-61d6-4998-8a58-11e712e67eac.jpg'
-        name='고양이 사진 공유방'
-      />
+      {group && <GroupInfoItem url={group.url} name={group.groupName} />}
       <MemberList />
     </>
   )
