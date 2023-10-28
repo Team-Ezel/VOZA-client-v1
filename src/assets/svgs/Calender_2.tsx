d72fb42e584a76f5fffabfd6f4dafe137e8afeab
이아론 <aron0613@naver.com>
2023-10-29 00:32:00 +0900

    :bento: Add Svg Calender_2

---
+++ src/assets/svgs/Calender_2.tsx
@@ -0,0 +1,24 @@
+export default function Calender_2() {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_1631_801)">
+                <mask id="path-1-inside-1_1631_801" fill="white">
+                    <path d="M2 5C2 4.44772 2.44772 4 3 4H21C21.5523 4 22 4.44772 22 5V20C22 21.1046 21.1046 22 20 22H4C2.89543 22 2 21.1046 2 20V5Z" />
+                </mask>
+                <path d="M2 5C2 4.44772 2.44772 4 3 4H21C21.5523 4 22 4.44772 22 5V20C22 21.1046 21.1046 22 20 22H4C2.89543 22 2 21.1046 2 20V5Z" stroke="#656B80" strokeWidth="4" mask="url(#path-1-inside-1_1631_801)" />
+                <mask id="path-2-inside-2_1631_801" fill="white">
+                    <path d="M2 5C2 4.44772 2.44772 4 3 4H21C21.5523 4 22 4.44772 22 5V10H2V5Z" />
+                </mask>
+                <path d="M2 5C2 4.44772 2.44772 4 3 4H21C21.5523 4 22 4.44772 22 5V10H2V5Z" stroke="#656B80" stroke-width="4" mask="url(#path-2-inside-2_1631_801)" />
+                <path d="M7 3V6" stroke="#656B80" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M17 3V6" stroke="#656B80" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+            <defs>
+                <clipPath id="clip0_1631_801">
+                    <rect width="24" height="24" fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+
+    )
+}
