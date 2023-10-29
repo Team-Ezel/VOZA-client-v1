e6add9185c8930ce5bdf3b3a8991e50448eda8fa
이아론 <aron0613@naver.com>
2023-10-29 16:54:38 +0900

    :lipstick:Add Toolbar List Data 추가

---
+++ src/assets/data/ToolbarListData.ts
@@ -0,0 +1,54 @@
+import * as I from "@/assets/svgs"
+
+export const ToolbarListData = [
+    {
+        img: I.Toolbar_H1,
+        content: "# 텍스트",
+        section: 1
+    },
+    {
+        img: I.Toolbar_H2,
+        content: "## 텍스트",
+        section: 1
+    },
+    {
+        img: I.Toolbar_H3,
+        content: "### 텍스트",
+        section: 1
+    },
+    {
+        img: I.Toolbar_H4,
+        content: "#### 텍스트",
+        section: 1
+    },
+    {
+        img: I.Toolbar_Bold,
+        content: "",
+        section: 2
+    },
+    {
+        img: I.Toolbar_Italic,
+        content: "",
+        section: 2
+    },
+    {
+        img: I.Toolbar_Strikethrough,
+        content: "",
+        section: 2
+    },
+    {
+        img: I.Toolbar_Link,
+        content: "",
+        section: 3
+    },
+    {
+        img: I.Toolbar_Image,
+        content: "",
+        section: 3
+    },
+    {
+        img: I.Toolbar_Code,
+        content: "",
+        section: 3
+    },
+]
