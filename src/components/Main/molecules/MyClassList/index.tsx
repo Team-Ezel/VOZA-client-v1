3cc55a56bf311debe101532c319a8e893a7f150a
이아론 <aron0613@naver.com>
2023-11-03 23:21:34 +0900

    :lipstick: Add: myClass로직작성

--- src/components/Main/molecules/MyClassList/index.tsx
+++ src/components/Main/molecules/MyClassList/index.tsx
@@ -1,26 +1,39 @@
-import { useState } from 'react';
-import * as S from './style';
-import { MyClassListPropsTypes } from '@/types/components/Main/MyClassType';
-import MyClassItem from '../../atoms/Item/MyClassItem';
-import NewClassItem from '../../atoms/Item/NewClass';
-function MyClassList ({datas}:MyClassListPropsTypes){
-    const [res,setRes] = useState(datas);
-    console.log(res)
-    return(
-        <S.MyClassList>
-            <NewClassItem/>
-            {res.map((item)=>(
-                <MyClassItem
-                id={item.id}
-                title={item.title}
-                leader={item.leader}
-                member={item.member}
-                thumbnail={item.thumbnail}
-                key={item.id}
-                />
-            ))}
-        </S.MyClassList>
-    )
+import { useEffect } from 'react'
+import * as S from './style'
+import { MyClassListPropsTypes } from '@/types/components/Main/MyClassType'
+import MyClassItem from '../../atoms/Item/MyClassItem'
+import NewClassItem from '../../atoms/Item/NewClass'
+import { MyClassData } from '@/types/components/Main/MyClassType'
+import useFetch from '@/hooks/useFetch'
+
+function MyClassList() {
+  const { fetch, isLoading, data } = useFetch<MyClassData>({
+    url: '/group/myGroups',
+    method: 'GET',
+  })
+
+  useEffect(() => {
+    fetch()
+    console.log(data)
+  }, [fetch])
+
+  return (
+    <S.MyClassList>
+      <NewClassItem />
+      {!isLoading &&
+        data &&
+        data.groupList.map((item) => (
+          <MyClassItem
+            id={item.groupId}
+            title={item.groupName}
+            leader={item.leaderName}
+            member={item.members}
+            thumbnail={item.url}
+            key={item.groupId}
+          />
+        ))}
+    </S.MyClassList>
+  )
 }
 
-export default MyClassList;
+export default MyClassList
--- src/components/Main/organisms/MyClass/index.tsx
+++ src/components/Main/organisms/MyClass/index.tsx
@@ -1,71 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react'
 import MyClassList from '../../molecules/MyClassList'
 import * as S from './style'
 
-const dummyListItem = {
-  data: {
-    groupList: [
-      {
-        id: 1,
-        title: '그룹이름',
-        leader: '리더이름',
-        member: 5,
-        thumbnail: '',
-      },
-      {
-        id: 2,
-        title: '그룹이름2',
-        leader: '리더이름2',
-        member: 7,
-        thumbnail: '',
-      }, {
-        id: 3,
-        title: '그룹이름',
-        leader: '리더이름',
-        member: 5,
-        thumbnail: '',
-      }, {
-        id: 4,
-        title: '그룹이름',
-        leader: '리더이름',
-        member: 5,
-        thumbnail: '',
-      }, {
-        id: 5,
-        title: '그룹이름',
-        leader: '리더이름',
-        member: 5,
-        thumbnail: '',
-      }, {
-        id: 6,
-        title: '그룹이름',
-        leader: '리더이름',
-        member: 5,
-        thumbnail: '',
-      }, {
-        id: 7,
-        title: '그룹이름',
-        leader: '리더이름',
-        member: 5,
-        thumbnail: '',
-      }, {
-        id: 8,
-        title: '그룹이름',
-        leader: '리더이름',
-        member: 5,
-        thumbnail: '',
-      },
-    ],
-  },
-}
+// {
+//   id: 1,
+//   title: '그룹이름',
+//   leader: '리더이름',
+//   member: 5,
+//   thumbnail: '',
+// },
 
 function MyClass() {
-    const [datas,setDatas] = useState(dummyListItem.data.groupList);
   return (
     <S.MyClassWrapper>
-      <MyClassList 
-      datas={datas}
-      />
+      <MyClassList />
     </S.MyClassWrapper>
   )
 }
