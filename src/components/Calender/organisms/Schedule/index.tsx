1586e8c157c105f27b5e12daea3c109cdaee17f3
이아론 <aron0613@naver.com>
2023-11-07 15:36:27 +0900

    ✨ Add: 일정 리스트 로직 작성

--- src/components/Calender/organisms/Schedule/index.tsx
+++ src/components/Calender/organisms/Schedule/index.tsx
@@ -1,17 +1,19 @@
-import { SchedulePropsType } from "@/types/components/Calender/ScheduleTypes";
-import CalenderChange from "../../molecules/CalenderChange"
-import ScheduleList from "../../molecules/ScheduleList"
-import * as S from "./style"
+import { SchedulePropsType } from '@/types/components/Calender/ScheduleTypes'
+import CalenderChange from '../../molecules/CalenderChange'
+import ScheduleList from '../../molecules/ScheduleList'
+import * as S from './style'
 
 export default function Schedule({ day, setDay }: SchedulePropsType) {
-    const jumpToDay = (num: number) => (num ? setDay(day.clone().add(1, 'day')) : setDay(day.clone().subtract(1, 'day')));
+  const jumpToDay = (num: number) =>
+    num
+      ? setDay(day.clone().add(1, 'day'))
+      : setDay(day.clone().subtract(1, 'day'))
 
-
-    return (
-        <S.ScheduleWrapper>
-            <CalenderChange month={day.format("DD")} onClick={jumpToDay} type="일" />
-            <S.Line />
-            <ScheduleList day={day.toString()} />
-        </S.ScheduleWrapper>
-    )
+  return (
+    <S.ScheduleWrapper>
+      <CalenderChange month={day.format('DD')} onClick={jumpToDay} type='일' />
+      <S.Line />
+      <ScheduleList day={day.format('yyyy-MM')} />
+    </S.ScheduleWrapper>
+  )
 }
