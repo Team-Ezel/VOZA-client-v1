5c7a2652b7fb245282d22756fd9771b400e22d73
윤수용 <yunsuyong739@naver.com>
2023-11-27 12:11:51 +0900

    :sparkles: Add : 멤버 일시정지 기능 추가

--- src/components/Member/atoms/Modal/MemberSuspensionModal/index.tsx
+++ src/components/Member/atoms/Modal/MemberSuspensionModal/index.tsx
@@ -4,14 +4,20 @@ import { createPortal } from 'react-dom'
 import Button from '@/components/Common/atoms/Button/Button'
 import { TimeFilterData } from '@/assets/data/TimeFilterData'
 import TimeItem from '../../ModalItems/TimeItem'
+import Router from 'next/router'
 
 type MemberSuspensionModalProps = {
   onClose: () => void
+  memberId: number
 }
 
 const MemberSuspensionModal: React.FC<MemberSuspensionModalProps> = ({
   onClose,
+  memberId,
 }) => {
+  const baseurl = process.env.NEXT_PUBLIC_BASEURL
+  const { id } = Router.query
+
   // 모달 바깥 영역을 클릭할 때 onClose 함수를 호출하여 모달을 닫음
   const handleModalClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
@@ -21,8 +27,35 @@ const MemberSuspensionModal: React.FC<MemberSuspensionModalProps> = ({
 
   const [selectedTime, setSelectedTime] = useState('') // 선택된 타임을 관리
 
-  const handleItemClick = (time: any) => {
+  const handleItemClick = (time: string) => {
     setSelectedTime(time) // 선택된 타임을 업데이트
+  }
+
+  const handleKickOutMember = async () => {
+    try {
+      const accessToken = localStorage.getItem('kakao-accessToken')
+
+      if (!selectedTime) {
+        alert('시간을 선택하세요.')
+        return
+      }
+
+      const apiUrl = `${baseurl}/group/${id}/member/${memberId}?kickOutTime=${selectedTime}`
+      const response = await fetch(apiUrl, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+
+      if (response.status === 204) {
+        // 멤버가 성공적으로 킥아웃됨
+        onClose()
+      }
+    } catch (error) {
+      // 네트워크 오류 또는 기타 예외 처리
+    }
   }
 
   return createPortal(
@@ -35,7 +68,8 @@ const MemberSuspensionModal: React.FC<MemberSuspensionModalProps> = ({
             <TimeItem
               time={item.time}
               key={item.uniqueId}
-              isSelected={item.time === selectedTime}
+              benTime={item.benTime}
+              isSelected={item.benTime === selectedTime}
               onItemClick={handleItemClick}
             />
           ))}
@@ -47,6 +81,7 @@ const MemberSuspensionModal: React.FC<MemberSuspensionModalProps> = ({
           background='#3355CD'
           color='#fff'
           border='none'
+          onClick={handleKickOutMember}
         >
           임시정지
         </Button>
