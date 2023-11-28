6be31a3db6b68dbecde978e5a9006bb9cd9cbe8c
이아론 <aron0613@naver.com>
2023-11-28 10:50:36 +0900

    :alien: Fix Change api

--- src/components/Main/organisms/RecommendClass/index.tsx
+++ src/components/Main/organisms/RecommendClass/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
-import { RecommendClassListType } from '@/types/components/Main/RecommendClassType'
+import { RecommendClassItemType } from '@/types/components/Main/RecommendClassType'
 import RecommendClassList from '../../molecules/RecommendClassList'
 import * as S from './style'
 import useFetch from '@/hooks/useFetch'
 
 function RecommendClass() {
-  const { data, isLoading, fetch } = useFetch<RecommendClassListType>({
-    url: `/group/recoGroups`,
+  const { data, isLoading, fetch } = useFetch<RecommendClassItemType[]>({
+    url: `/group/recommended`,
     method: 'GET',
   })
 
@@ -17,9 +17,7 @@ function RecommendClass() {
     <S.RecommendClassWrapper>
       <S.RecommendClass>
         <S.titleWrapper>추천모임</S.titleWrapper>
-        {!isLoading && data ? (
-          <RecommendClassList groupList={data.groupList} />
-        ) : null}
+        {!isLoading && data ? <RecommendClassList groupList={data} /> : null}
       </S.RecommendClass>
     </S.RecommendClassWrapper>
   )
