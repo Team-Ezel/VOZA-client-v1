20e6f2bb09b716cf5ac0b67b2a554a0233ba6751
윤수용 <yunsuyong739@naver.com>
2023-12-04 15:32:12 +0900

    :lipstick: Add : checked에셋 추가

---
+++ src/assets/svgs/Checked.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Checked = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+    >
+      <path
+        d='M4.5 12.75L10.5 18.75L19.5 5.25'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
+
+export default Checked
