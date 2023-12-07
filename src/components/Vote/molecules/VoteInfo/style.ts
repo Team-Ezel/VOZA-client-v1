018c6ca04e4c5c9a2a6974f5a9e40c618b54cd61
윤수용 <yunsuyong739@naver.com>
2023-12-07 15:18:55 +0900

    :sparkles: Create Vote View

---
+++ src/components/Vote/molecules/VoteInfo/style.ts
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled'
+
+export const PostInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1em;
+`
+
+export const PostTitle = styled.h1`
+  font-size: 40px;
+  width: 50em;
+  word-wrap: break-word;
+  margin-bottom: 1em;
+`
+
+export const PostInfoOrganism = styled.div`
+  margin-top: auto;
+  margin-left: auto;
+  width: 15em;
+  margin-bottom: 0.5em;
+`
+
+export const PostInfoContent = styled.div`
+  font-size: 18px;
+  color: #999999;
+  text-align: right;
+  width: 13em;
+`
+export const EditArea = styled.textarea`
+  font-size: 40px;
+  width: 50em;
+  resize: none;
+  height: 1.5em;
+  font-weight: 700;
+  margin-bottom: 1em;
+`
