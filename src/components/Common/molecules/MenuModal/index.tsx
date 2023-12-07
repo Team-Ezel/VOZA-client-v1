9cbbe77db1d63cc8707cf94ba3e2a15a3a56ddb9
윤수용 <yunsuyong739@naver.com>
2023-12-07 14:52:26 +0900

    :lipstick: :Member MenuBar 수정

--- src/components/Common/molecules/MenuModal/index.tsx
+++ src/components/Common/molecules/MenuModal/index.tsx
@@ -20,15 +20,6 @@ const MenuModal = ({
 
   return (
     <S.MenuModalWrapper>
-      <Link href={'/'}>
-        <p>마이페이지</p>
-      </Link>
-      <Link href={'/'}>
-        <p>친구추가</p>
-      </Link>
-      <Link href={'/'}>
-        <p>친구초대</p>
-      </Link>
       <p onClick={joinGroup}>그룹가입</p>
       <Link href={'/'}>
         <p>로그아웃</p>
